aede0bc0ba3eed88758b0deb769ad5d2d0027abb
shikhasharma0 <shikha0252@gmail.com>
2022-08-02 15:46:18 +0530

    Done

--- src/Model/UrlModel.js
+++ src/Model/UrlModel.js
@@ -2,9 +2,19 @@ const mongoose = require("mongoose");
 
 
 const urlSchema = new mongoose.Schema({
-    longUrl: {type: String,require:true},
-    shortUrl: {type: String,require:true,unique:true} ,
-    urlCode: {type: String, require:true, unique:true, lowercase:true, trim:true }
+    longUrl: {
+        type: String,
+        require:true},
+    shortUrl: {
+        type: String,
+        require:true,
+        unique:true} ,
+    urlCode: {
+        type: String,
+        require:true,
+         unique:true,
+         lowercase:true, 
+         trim:true }
 }, { timestamps: true })
 
 module.exports = mongoose.model('Url', urlSchema)
